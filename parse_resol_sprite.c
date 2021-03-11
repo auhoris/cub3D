582243fbaf4d2b54aec5ad12fb2ed816dcfa8e01
Ariane Uhoris <auhoris@am-l2.msk.21-school.ru>
2021-03-11 19:41:40 +0300

    fixed int

--- parse_resol_sprite.c
+++ parse_resol_sprite.c
@@ -6,14 +6,53 @@
 /*   By: auhoris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 17:07:55 by auhoris           #+#    #+#             */
-/*   Updated: 2021/03/03 17:07:56 by auhoris          ###   ########.fr       */
+/*   Updated: 2021/03/11 18:00:22 by auhoris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "libft/libft.h"
 #include <stdio.h>
 
-int	parse_resol(t_config *all, char **split)
+static int	set_y(t_config *all, char **split)
+{
+	int	i;
+
+	i = -1;
+	while (!in_set("123456789", split[2][++i]))
+		;
+	if (ft_strlen(&split[2][i]) > 5)
+	{
+		all->args->res_y = all->screen_height;
+		return (OK);
+	}
+	all->args->res_y = ft_atoi(split[2]);
+	all->args->res_y = (all->args->res_y > all->screen_height)
+		? all->args->res_y = all->screen_height : all->args->res_y;
+	all->args->res_y = all->args->res_y < 1 ? 480 : all->args->res_y;
+	return (OK);
+}
+
+static int	set_x(t_config *all, char **split)
+{
+	int	i;
+
+	i = -1;
+	while (!in_set("123456789", split[1][++i]))
+		;
+	if (ft_strlen(&split[1][i]) > 5)
+	{
+		all->args->res_x = all->screen_width;
+		return (OK);
+	}
+	all->args->res_x = ft_atoi(split[1]);
+	all->args->res_x = (all->args->res_x > all->screen_width)
+		? all->args->res_x = all->screen_width : all->args->res_x;
+	all->args->res_x = all->args->res_x < 1 ? 640 : all->args->res_x;
+	return (OK);
+}
+
+int			parse_resol(t_config *all, char **split)
 {
 	int	i;
 	int	str;
@@ -28,13 +67,7 @@ int	parse_resol(t_config *all, char **split)
 			if (!in_set("-+0123456789", split[i][str]))
 				return (ERROR);
 	}
-	all->args->res_x = ft_atoi(split[1]);
-	all->args->res_y = ft_atoi(split[2]);
-	all->args->res_x = (all->save == 0 && all->args->res_x > all->screen_width)
-		? all->args->res_x = all->screen_width : all->args->res_x;
-	all->args->res_y = (all->save == 0 && all->args->res_y > all->screen_height)
-		? all->args->res_y = all->screen_height : all->args->res_y;
-	all->args->res_x = all->args->res_x < 1 ? 640 : all->args->res_x;
-	all->args->res_y = all->args->res_y < 1 ? 480 : all->args->res_y;
+	set_x(all, split);
+	set_y(all, split);
 	return (OK);
 }
